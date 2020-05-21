b42619ca2eeaf9d27edea702f8d9c4343f5765a3
Luiz Felipe Ribeiro <piporibeiro@gmail.com>
2020-05-21 11:02:44 -0300

    Novos cpnteúdos
    Novos conteúdos de node.js, usando exios

---
+++ Node/instancia_cliente.js
@@ -0,0 +1,13 @@
+const contadorA = require('./instancia_unica')
+const contadorB = require('./instancia_unica')
+
+const contadorC = require('./instancia_nova')()
+const contadorD = require('./instancia_nova')()
+
+contadorA.inc()
+contadorA.inc()
+console.log(contadorA.valor, contadorB.valor);
+
+contadorC.inc()
+contadorC.inc()
+console.log(contadorC.valor, contadorD.valor);
---
+++ Node/instancia_unica.js
@@ -0,0 +1,8 @@
+// Node faz cache dos módules
+
+module.exports = {
+  valor: 1,
+  inc(){
+    this.valor++
+  }
+}
