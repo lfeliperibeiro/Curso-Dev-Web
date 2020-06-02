9433a4e33ed9d80535836bbc9db23492a7d4526e
Luiz Felipe Ribeiro <piporibeiro@gmail.com>
2020-06-01 23:16:11 -0300

    Gulp
    Aulas e criação de uma SPA utilizando Gulp, sass, e JS

---
+++ gulp/spa/gulpfile.js
@@ -0,0 +1,31 @@
+const {
+    series,
+    parallel
+} = require('gulp')
+const gulp = require('gulp')
+
+const {
+    appHTML,
+    appCSS,
+    appJS,
+    appIMG
+} = require('./gulpTasks/app')
+
+const {
+    depsCSS,
+    depsFonts
+} = require('./gulpTasks/deps')
+
+const {
+    monitoraArquivos,
+    servidor
+} = require('./gulpTasks/servidor')
+
+module.exports.default = series(
+    parallel(
+        series(appHTML, appCSS, appJS, appIMG),
+        series(depsCSS, depsFonts)
+    ),
+    servidor,
+    monitoraArquivos
+)
